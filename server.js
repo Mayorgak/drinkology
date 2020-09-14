86f94af24e5c50cfbe2207406aea3a15d321de2d
Kristen M <kristenmayorga@gmail.com>
2020-09-13 20:22:28 -0400

    updates

--- server.js
+++ server.js
@@ -6,18 +6,6 @@ const session = require('express-session');
 
 const PORT = process.env.PORT || 3001;
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
-// app.use(session(sess));
-
-
 const app = express(); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -36,6 +24,6 @@ app.set('view engine', 'handlebars');
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
