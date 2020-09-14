3a34cca66c1091f79b2f8afdfcf76ef6f2aa355e
Leo Ayala <leo_a1016@yahoo.com>
2020-09-13 20:12:16 -0400

    changed main layout and added CSS

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,12 +1,14 @@
-function logout() {
-  fetch("/api/user/logout", {
-    method: "post",
-    headers: { "Content-Type": "application/json" },
-  })
-    .then(function () {
-      document.location.replace("/");
-    })
-    .catch((err) => console.log(err));
+async function logout() {
+  const response = await fetch('/api/users/logout', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
 }
 
-document.querySelector("#logout-link").addEventListener("click", logout);
+document.querySelector('#logout').addEventListener('click', logout);
